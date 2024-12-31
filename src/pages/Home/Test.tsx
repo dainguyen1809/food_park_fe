9d79a55c36442b5d9f5df373dab668eeeceda710
dainguyen.nhd <dainguyen.nhd@gmail.com>
2024-12-31 23:10:33 +0700

    feat: login with google & facebook

--- src/pages/Home/Test.tsx
+++ src/pages/Home/Test.tsx
@@ -1,3 +1,11 @@
 export default function Test() {
+  // if (window.location.hash === '#_=_') {
+  //   window.history.replaceState(
+  //     {},
+  //     document.title,
+  //     window.location.pathname + window.location.search
+  //   );
+  // }
+
   return <div>Welcome</div>;
 }
